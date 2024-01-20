1d7c33309adc59b7244a379ceedc746123087058
ikeawesom <ikelimjl@gmail.com>
2024-01-20 20:55:53 +0800

    added games list

---
+++ src/constants.ts
@@ -0,0 +1,37 @@
+const GAMES = [
+  {
+    Name: "In-Between",
+    Description:
+      "A simple betting game where players guess if the next card drawn will fall between the two existing cards.",
+    "Difficulty Level": "Easy",
+    "Number of Players": "3 or more",
+  },
+  {
+    Name: "Poker",
+    Description:
+      "A popular gambling game with various variants, involving skill, strategy, and bluffing.",
+    "Difficulty Level": "Moderate to Hard",
+    "Number of Players": "2 or more",
+  },
+  {
+    Name: "Blackjack",
+    Description:
+      "A comparing card game between one or more players and a dealer, where each player competes against the dealer.",
+    "Difficulty Level": "Moderate",
+    "Number of Players": "2 or more",
+  },
+  {
+    Name: "Baccarat",
+    Description:
+      "A comparing card game played between two hands - the 'player' and the 'banker'.",
+    "Difficulty Level": "Moderate",
+    "Number of Players": "2 or more",
+  },
+  {
+    Name: "Big Two",
+    Description:
+      "A shedding-type card game where the goal is to be the first to play all of one's cards.",
+    "Difficulty Level": "Moderate",
+    "Number of Players": "4",
+  },
+];
